c2bc8165dd1269398e6ad5a9098cbe7f8e4670ba
ngoctam <nguyenngoctamvn@gmail.com>
2018-02-25 14:54:35 +0700

    Learned: Streams and Pipes

--- app_section_6.js
+++ app_section_6.js
@@ -61,12 +61,65 @@ console.log(greet);
 
 var greet2 = fs.readFile(__dirname + '/greet.txt', function(err, data){
     // err -> error first callback
-    console.log(data);
+    if (err == null)
+        console.log(data);
 });
 
 var greet2 = fs.readFile(__dirname + '/greet.txt', 'utf8', function(err, data){
     // err -> error first callback
-    console.log(data);
+    if (err == null)
+        console.log(data);
 });
 
 console.log('Done!');
+
+/*
+*** Lecture 51: Streams
+*/
+console.log('\n *** Lecture 51: Streams');
+
+var fs = require('fs');
+var readable = fs.createReadStream(__dirname + '/greet2.txt');
+
+readable.on('data', function(chunk) {
+    console.log(chunk);
+});
+
+var readable = fs.createReadStream(__dirname + '/greet2.txt', { encoding: 'utf8' });
+
+readable.on('data', function(chunk) {
+    console.log(chunk.length);
+});
+
+var readable = fs.createReadStream(__dirname + '/greet2.txt', { encoding: 'utf8', highWaterMark: 32*1024 });
+
+readable.on('data', function(chunk) {
+    console.log(chunk.length);
+});
+
+var readable = fs.createReadStream(__dirname + '/greet2.txt', { encoding: 'utf8', highWaterMark: 32*1024 });
+var writeable = fs.createWriteStream(__dirname + '/greet2_copy.txt');
+
+readable.on('data', function(chunk) {
+    // console.log(chunk);
+    writeable.write(chunk);
+});
+
+/*
+*** Lecture 53: Pipes
+*/
+console.log('\n *** Lecture 53: Pipes');
+
+var fs = require('fs');
+var zlib = require('zlib');
+
+var readable2 = fs.createReadStream(__dirname + '/greet2.txt', { encoding: 'utf8', highWaterMark: 16*1024 });
+var writeable2 = fs.createWriteStream(__dirname + '/greet2_copy_2.txt');
+
+readable2.pipe(writeable2);
+
+var compressed = fs.createWriteStream(__dirname + '/greet2_copy_2.txt.gz');
+var gzip = zlib.createGzip();
+
+readable2.pipe(gzip).pipe(compressed); // method chaining
+
