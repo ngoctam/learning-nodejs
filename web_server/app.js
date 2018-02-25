85da35fb967338661ba7fde0226f3027612668f1
ngoctam <nguyenngoctamvn@gmail.com>
2018-02-25 15:57:45 +0700

    Web server: Outputting HTML And Templates

--- web_server/app.js
+++ web_server/app.js
@@ -1,8 +1,15 @@
 var http = require('http');
+var fs = require('fs');
 
-http.createServer(function (req, res) {
-
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-
+http.createServer(function(req, res) {
+    
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    
+    var html = fs.readFileSync(__dirname + '/index.htm', 'utf8');
+    var message = 'Hello world...';
+    
+    html = html.replace('{Message}', message);
+    
+    res.end(html);
+    
 }).listen(1337, '127.0.0.1');
