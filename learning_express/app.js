168a641319c7928f8e3cc51296e09b7e6ae17060
ngoctam <nguyenngoctamvn@gmail.com>
2018-03-03 07:39:42 +0700

    Use Template with ejs and Express

--- learning_express/app.js
+++ learning_express/app.js
@@ -5,19 +5,29 @@ var port = process.env.PORT || 3000;
 
 app.use('/assets', express.static(__dirname + '/public'));
 
+app.set('view engine', 'ejs');
+
 app.use('/', function(req, res, next) {
     console.log('Request Url: ' + req.url);
     next();
 });
 
 app.get('/', function (req, res) {
-    console.log('Accepted from middleware !');
-    res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello World !!!</h1></body></html>');
+    res.render('index');
 });
 
 app.get('/person/:id', function (req, res) {
-    res.send('<html><head></head><body><h1>Person : ' + req.params.id +  '</h1></body></html>');
+    res.render('person', { ID: req.params.id });
 });
+
+// app.get('/', function (req, res) {
+//     console.log('Accepted from middleware !');
+//     res.send('<html><head><link href="assets/style.css" type="text/css" rel="stylesheet" /></head><body><h1>Hello World !!!</h1></body></html>');
+// });
+
+// app.get('/person/:id', function (req, res) {
+//     res.send('<html><head></head><body><h1>Person : ' + req.params.id +  '</h1></body></html>');
+// });
 
 app.get('/api', function (req, res) {
     res.json( { firstname: 'John', lastname: 'Doe'} );
