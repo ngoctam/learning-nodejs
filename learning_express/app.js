74a401a5ff114849547c79c0ec2a964a20405bb2
ngoctam <nguyenngoctamvn@gmail.com>
2018-03-03 07:23:04 +0700

    Use route and middleware with Express

--- learning_express/app.js
+++ learning_express/app.js
@@ -3,9 +3,22 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-    res.send("<html><head></head><body><h1>Hello World !!!</h1></body></html>")
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.use('/', function(req, res, next) {
+    console.log('Request Url: ' + req.url);
+    next();
 });
+
+app.get('/', function (req, res) {
+    console.log('Accepted from middleware !');
+    res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello World !!!</h1></body></html>');
+});
+
+app.get('/person/:id', function (req, res) {
+    res.send('<html><head></head><body><h1>Person : ' + req.params.id +  '</h1></body></html>');
+});
+
 app.get('/api', function (req, res) {
     res.json( { firstname: 'John', lastname: 'Doe'} );
 });
