2e4bdea1ddb61b68a2bea2628a240417054302b2
ngoctam <nguyenngoctamvn@gmail.com>
2018-03-03 20:27:35 +0700

    Structuring an App

--- learning_express/app.js
+++ learning_express/app.js
@@ -1,13 +1,10 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
 
-var port = process.env.PORT || 3000;
+var apiController = require('./controllers/apiController');
+var htmlController = require('./controllers/htmlController');
 
-// npm install body-parser --save
-// create application/json parser
-var jsonParser = bodyParser.json();
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var port = process.env.PORT || 3000;
 
 app.use('/assets', express.static(__dirname + '/public'));
 
@@ -18,20 +15,6 @@ app.set('view engine', 'ejs');
 app.use('/', function(req, res, next) {
     console.log('Request Url: ' + req.url);
     next();
-});
-
-app.get('/', function (req, res) {
-    res.render('index');
-});
-
-app.get('/person/:id', function (req, res) {
-    res.render('person', { ID: req.params.id, Qstr: req.query.qstr });
-});
-
-app.post('/person', urlencodedParser, function (req, res) {
-    res.send('Thank you!');  
-    console.log(req.body.firstname);
-    console.log(req.body.lastname);
 });
 
 // app.post('/personjson', jsonParser, function (req, res) {
@@ -53,17 +36,8 @@ app.post('/person', urlencodedParser, function (req, res) {
 //     res.json( { firstname: 'John', lastname: 'Doe'} );
 // });
 
-app.get('/api/person/:id', function (req, res) {
-    // get that data from database
-    res.json( { firstname: 'John', lastname: 'Doe'} );
-});
+htmlController(app);
 
-app.post('/api/person', jsonParser, function (req, res) {
-    // save to the database
-});
-
-app.delete('/api/person/:id', function (req, res) {
-    // delete from the database
-});
+apiController(app);
 
 app.listen(port);
